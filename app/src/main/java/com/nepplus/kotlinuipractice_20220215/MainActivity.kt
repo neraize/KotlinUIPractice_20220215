9508aaf4b87d7cb72b3618524bcb93bbdafd0052
seojungmin <neraize@gmail.com>
2022-02-15 15:08:30 +0900

    get기능 +조건문활용

--- app/src/main/java/com/nepplus/kotlinuipractice_20220215/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlinuipractice_20220215/MainActivity.kt
@@ -20,6 +20,21 @@ class MainActivity : AppCompatActivity() {
 
             // 담겨있는 입력문구를 ->텍스트뷰의 text속성의 값으로 대입
             txtMessage.text=inputMessage  // TextView의 문구(text)를 변경 :set
+
+
+            btnCallculate.setOnClickListener {
+
+                // 입력한 숫자 추출
+                val inputNumber = edtNumber.text.toString().toInt()  //입력한 문구를 -> String으로 ->  Int로 변환
+
+                //숫자는 짝수인가? -> 조건문 if문 활용
+                if(inputNumber%2==0){
+                    Toast.makeText(this, "입력한 숫자는 짝수입니다", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    Toast.makeText(this, "입력한 숫자는 홀수입니다", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
