23db49f1ea041ccb7a8976da3a205545c5f05114
seojungmin <neraize@gmail.com>
2022-02-15 14:53:06 +0900

    입력한 메세지를  -> 텍스트뷰의 문구로도 반영

--- app/src/main/java/com/nepplus/kotlinuipractice_20220215/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlinuipractice_20220215/MainActivity.kt
@@ -10,11 +10,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnOk.setOnClickListener {
+        //버튼이 눌릴때 -> 입력된 메세지가 어떤건지 추출 -> 토스트로 띄어주기 + 텍스트뷰의 문구로 반영하기
 
-        val inputMessage =editMessage.text.toString()
+        btnOk.setOnClickListener {
+        //버튼이 눌릴때 마다 실행
+        // ->입력된 메세지는? 변수에 담아두자
+
+        val inputMessage =editMessage.text.toString()  //EditText의 문구(text) -> String으로 변환
             Toast.makeText(this, inputMessage, Toast.LENGTH_SHORT).show()
 
+            //
+
+            txtMessage.text=inputMessage  // TextView의 문구(text)를 변경 :set
         }
     }
 }
