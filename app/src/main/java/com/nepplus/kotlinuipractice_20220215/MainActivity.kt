3115a0e86df39f08bd5f97cefa494bb957f18751
seojungmin <neraize@gmail.com>
2022-02-15 14:45:33 +0900

    confrim  pressed  -> inputMessage value created  -> editMessage`s things -> transfer String  -> toast  display

--- app/src/main/java/com/nepplus/kotlinuipractice_20220215/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlinuipractice_20220215/MainActivity.kt
@@ -2,10 +2,19 @@ package com.nepplus.kotlinuipractice_20220215
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnOk.setOnClickListener {
+
+        val inputMessage =editMessage.text.toString()
+            Toast.makeText(this, inputMessage, Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
